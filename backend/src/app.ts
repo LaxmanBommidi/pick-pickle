42454f9cd199719a7bb99193fdf61dcb6544a5ce
LaxmanBommidi <112493885+LaxmanBommidi@users.noreply.github.com>
2026-05-31 22:21:47 +0530

    fix landing API response

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,12 @@ const app = express();
 app.use(express.json());
 
 app.get("/" , (req,res) => {
-    return res.json("hello World");
+    return res.status(200).json({
+        "name" : "pick-pickle api",
+        "version" : "1.0.0",
+        "message" : "Welcome to Pick-Pickle API",
+        "base_url" : "pick-pickle.vercel.app/"
+    });
 });
 
 app.use("/api/users" , userRoute);
